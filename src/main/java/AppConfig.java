ad36524859b33bbaa6e9e719489f2a6224f3f318
Ken Christie <javawebken@gmail.com>
2020-06-01 11:05:45 -0600

    End of Chapter 4

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,28 @@
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+//@ComponentScan({"com.pluralsight"})
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+        //service.setRepository(getSpeakerRepository());
+        return service;
+        //return new SpeakerServiceImpl();
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
+}
